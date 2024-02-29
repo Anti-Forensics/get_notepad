5bace2d4a96302c8b68ba6671b34ada49c7f0c3b
Jesse Shelley <realjesseshelley@gmail.com>
2024-02-29 13:30:07 -0700

    file data

--- get_notepad.py
+++ get_notepad.py
@@ -25,7 +25,7 @@ class GetNotepad:
         return selected_files
 
     def get_file_content(self, full_file_path: str) -> bytes:
-        with open(full_file_path, 'rb') as file:
+        with open(full_file_path, 'br') as file:
             return file.read()
 
     def get_file_name(self, full_file_path: str) -> str | None:
@@ -45,32 +45,30 @@ class GetNotepad:
         if type(issaved is bool):
             return issaved
 
-    def get_file_data(self, full_file_path: str) -> str:
+    def get_file_data(self, full_file_path: str) -> bytes:
         full_file = self.get_file_content(full_file_path)
         pattern = b"\x01\x00\x00\x00"
-        #pattern = b"\x00t\x00e"
-        #data_start = full_file[full_file.index(pattern)+5:-5]
-        data_start = full_file[full_file.index(pattern)+6:-5]
-        print(data_start)
-        # for data in data_start:
-        #     print(data)
-        print(data_start.decode('utf-16'))
+
+        data_start = full_file.index(pattern)
+        parsed_full = full_file[data_start + 6:-5]
+
+        try:
+            parsed_full_encode = parsed_full.decode('utf-16le').encode('utf8')
+        except:
+            parsed_full = full_file[data_start + 5: -5]
+            parsed_full_encode = parsed_full.decode('utf-16le').encode('utf8')
+
+        return parsed_full_encode
 
 
 def main():
     getnote = GetNotepad()
-    file = r"C:\Users\jesse\AppData\Local\Packages\Microsoft.WindowsNotepad_8wekyb3d8bbwe\LocalState\TabState\a1ebe41d-2862-4f3a-881e-299125f61e10.bin"
-   # files = getnote.get_all_files()
-   # for file in files:
-    #     #print(getnote.get_file_name(file))
-    #     # print(getnote.get_issaved(file))
-    print(getnote.get_file_name(file))
-    getnote.get_file_data(file)
 
-    # file = getnote.get_file_content("C:\\Users\\jesse\\AppData\\Local\\Packages\\Microsoft.WindowsNotepad_8wekyb3d8bbwe\\LocalState\\TabState\\02cd1a99-c190-4b10-854b-f9321717d3da.bin")
-    #
-    # print(file.decode('utf-16'))
-    # getnote.get_file_name("C:\\Users\\jesse\\AppData\\Local\\Packages\\Microsoft.WindowsNotepad_8wekyb3d8bbwe\\LocalState\\TabState\\02cd1a99-c190-4b10-854b-f9321717d3da.bin")
+    files = getnote.get_all_files()
+    for file in files:
+        print(getnote.get_file_name(file))
+        print(getnote.get_issaved(file))
+        print(getnote.get_file_data(file))
 
 
 if __name__ == "__main__":
